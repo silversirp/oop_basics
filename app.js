cd3708599ed646abcd06c97af9219953243b196c
Silver Sirp <silver.sirp@khk.ee>
2021-11-11 16:41:08 +0200

    Using constructor function

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-function createNewPerson(name) {
-    const obj = {};
-    obj.name = name;
-    obj.greeting = function() {
-        alert('Hi! I\'m ' + obj.name + '.');
+function Person(name) {
+    this.name = name;
+    this.greeting = function() {
+        alert('Hi! I\'m ' + this.name + '.');
     };
-    return obj;
 }
 
+let person1 = new Person('Bob');
+let person2 = new Person('Sarah');
+
+
 /*
-// use this in Console
+// use this in Console when using normal function
 const salva = createNewPerson('Salva');
 salva.name;
 salva.greeting();
