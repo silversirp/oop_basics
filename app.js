e0cca54b8fb2b77534061e9ac31358732f98f6f3
Silver Sirp <silver.sirp@khk.ee>
2021-11-11 16:59:52 +0200

    Using constructor function

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-function Person(name) {
-    this.name = name;
+function Person(first, last, age, gender, interests) {
+    this.name = {
+        first : first,
+        last : last
+    };
+    this.age = age;
+    this.gender = gender;
+    this.interests = interests;
+    this.bio = function() {
+        alert(this.name.first + ' ' + this.name.last + ' is ' + this.age + ' years old. He likes ' + this.interests[0] + ' and ' + this.interests[1] + '.');
+    };
     this.greeting = function() {
-        alert('Hi! I\'m ' + this.name + '.');
+        alert('Hi! I\'m ' + this.name.first + '.');
     };
 }
 
-let person1 = new Person('Bob');
-let person2 = new Person('Sarah');
+
+let person1 = new Person('Bob', 'Smith', 32, 'male', ['music', 'skiing']);
+person1.greeting();
+person1.bio();
 
 
 /*
